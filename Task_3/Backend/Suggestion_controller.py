770e8261a6d153c91a6e0db88f2193848c209ae6
ConcreteAI-Intern <internconcreteai@gmail.com>
2024-11-16 01:43:37 +0800

    added the function for the model

--- Task_3/Backend/Suggestion_controller.py
+++ Task_3/Backend/Suggestion_controller.py
@@ -2,8 +2,54 @@ import torch
 import numpy as np
 from utils import calculate_displacement
 
+import pandas as pd
+import numpy as np 
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
+
 # Load the model 
-#model = torch.load('')
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size=2, hidden_size=64, output_size=2):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, lengths):
+        packed_input = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+        packed_output, (hn, cn) = self.lstm(packed_input)
+        unpacked_output, _ = pad_packed_sequence(packed_output, batch_first=True)
+        out = self.fc(hn[-1])
+        return out
+    
+    
+def make_multiple_predictions(input_sequence, num_predictions, city):
+
+    city = city.upper()
+
+    input_tensor = torch.tensor(input_sequence).unsqueeze(0).float() 
+
+    model = LSTMModel()
+    model.load_state_dict(torch.load(f'./weights/city{city}.pth'))
+    model.eval()
+
+    seq_length = torch.tensor([len(input_sequence)])  
+
+    extended_sequence = input_sequence.copy()
+
+    with torch.no_grad():
+        for _ in range(num_predictions):
+            predicted_next = model(input_tensor, seq_length)
+            predicted_next = predicted_next.squeeze().cpu().numpy()  
+            predicted_next = [round(coordinate) for coordinate in predicted_next]  
+
+            extended_sequence.append(predicted_next)
+
+            input_tensor = torch.tensor(extended_sequence[-len(input_sequence):]).unsqueeze(0).float()
+            seq_length = torch.tensor([len(input_tensor[0])])
+
+    return extended_sequence
 
 
 def get_suggestions(history , city):
@@ -12,7 +58,7 @@ def get_suggestions(history , city):
     Get suggestions based on the provided x and y coordinates.
 
     Args:
-        coordinates (list): A list of (x, y) coordinates.
+        history (list): A list of (x, y) coordinates.
         city (str): The city name.
 
     
@@ -20,9 +66,10 @@ def get_suggestions(history , city):
     Returns:
         list: A list of suggestions.
     """
+
     # Placeholder for model integration, the model should return a list of suggestions in (x,y,category) format
     # Convert predictions to a list of suggestions
-    predictions = []#model.predict(history, city)
+    predictions = make_multiple_predictions(history, 5, city)
     suggestions = []
     for prediction in predictions:
         suggestion = {
